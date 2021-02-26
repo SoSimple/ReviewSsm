f43b97c1d701874d030cefafcc0822c0febd51a8
1962015745@qq.com <1962015745@qq.com>
2021-02-26 08:59:17 +0800

    20210219 commit

--- ReviewSsm/src/main/java/com/qjh/controller/ThreadDemo2.java
+++ ReviewSsm/src/main/java/com/qjh/controller/ThreadDemo2.java
@@ -2,6 +2,12 @@ package com.qjh.controller;
 
 /**
  * 通过实现Runnable接口实现多线程
+ *
+ * 实现Runnable接口比继承Thread类所具有的优势
+ *1、适合多个相同的程序代码的线程去处理同一资源
+ *2、可以避免java中单继承的限制
+ *3、增加程序的健壮性，代码可以被多个线程共享、代码和数据独立
+ *4、线程池只能放入实现Runnable或Callable类线程，不能直接放入继承Thread的类
  */
 public class ThreadDemo2 implements Runnable{
     @Override
